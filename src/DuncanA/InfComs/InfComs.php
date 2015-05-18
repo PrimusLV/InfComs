7acb459a9ac477fe6b53d4bc75b82fdd363807fe
PrimusLV <k.drivnieks@inbox.lv>
2015-05-18 15:58:53 +0300

    Create InfComs.php
    Forgot to add one namespace ^^

--- src/DuncanA/InfComs/InfComs.php
+++ src/DuncanA/InfComs/InfComs.php
@@ -5,23 +5,43 @@ namespace DuncanA\InfComs;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
 
 class InfComs extends PluginBase{
 
    public function onEnable () {
-         $this->getLogger()->info("onEnable() has been called!");
+         $this->getLogger()->info("Enabled.");
    }
    
    public function onDisable() {
-         $this->getLogger()->info("onDisable() has been called!");
+         $this->getLogger()->info("Disabled.");
    }
    
    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
-    if(strtolower($command->getName()) === "test"){
-        // Execute logic
-        return true;
+       switch($command->getName()){
+          case "quit":
+            if($sender instanceof Player){
+               if($sender->hasPermission("myplugin.quit"){
+                  $sender->kick();
+             }else{
+             	$sender->sendMessage("You don't have permission!");
+             	return true;
+             }
+          }else{
+          	$sender->sendMessage("Please run this command in-game!");
+          	return true;
+          }
+          case "info"
+          if($sender->hasPermission("myplugin.info")){
+          	$sender->sendMessage("Info Info Info...");
+          	$sender->sendMessage("Info Info Info...");
+          	$sender->sendMessage("Info Info Info...");
+          	return true;
+          }else{
+          	$sender->sendMessage("You don't have permission!");
+          	return true;
+          }
+       }
     }
-
-    return false;
-   }
+  }
 }
